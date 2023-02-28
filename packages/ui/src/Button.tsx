1f57e513a30dda4d11b732e6d605dcc9a284f8b7
Pablo_채호경 <hkchae@maxst.com>
2023-02-28 09:27:42 +0900

    :tada: first version
    this is first version

---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+export const Button = () => {
+  return <button className="ui-bg-pink-300 ui-p-4 ui-text-xl">Boop</button>;
+};
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,2 +1,4 @@
+import "./styles.css";
+
 import * as React from "react";
 export * from "./Button";
